918f700d4b6ed1a438d09e78819c87fb86ab05ae
rajeshkumar1612169 <rajeshkumar1612169@gmail.com>
2024-08-02 01:40:10 +0500

    Add Shadcn Components

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../../components/ui/button";
+
+<Button>Click me</Button>;
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
